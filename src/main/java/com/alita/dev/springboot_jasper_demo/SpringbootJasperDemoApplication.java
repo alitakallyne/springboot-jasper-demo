8002dc60118cf7749b1fbb0e316948af2159a67b
alitakallyne <alytakallyne@gmail.com>
2026-02-24 07:47:25 -0300

    Projeto inicial Spring Boot + JasperReports

---
+++ src/main/java/com/alita/dev/springboot_jasper_demo/SpringbootJasperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alita.dev.springboot_jasper_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJasperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJasperDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alita/dev/springboot_jasper_demo/SpringbootJasperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alita.dev.springboot_jasper_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJasperDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
